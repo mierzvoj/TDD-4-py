85f4a42c20b8121956b9668b0f0aedafb26c9c37
wojciech mierzejewski <55141520+mierzvoj@users.noreply.github.com>
2022-12-07 18:22:08 +0100

    test range 7

--- Tests.py
+++ Tests.py
@@ -125,3 +125,24 @@ class ChristmasCarol(unittest.TestCase):
                          "six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, "
                          "and a Partridge in a Pear Tree."
                          )
+
+
+    def test_ver_range_1_to_8(self):
+        self.assertEqual(starts_ends(0, 7),
+                         "On the first day of Christmas, my true love gave to me: a Partridge in a Pear Tree."
+                         "On the second day of Christmas, my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree."
+                         "On the third day of Christmas, my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+                         "On the fourth day of Christmas, my true love gave to me: "
+                         "four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+                         "On the fifth day of Christmas, my true love gave to me: five Gold Rings, four Calling Birds, "
+                         "three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+                         "On the sixth day of Christmas, my true love gave to me: six Geese-a-Laying, five Gold Rings, "
+                         "four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+                         "On the seventh day of Christmas, my true love gave to me: seven Swans-a-Swimming, "
+                         "six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, "
+                         "and a Partridge in a Pear Tree."
+                         "On the eighth day of Christmas, my true love gave to me: eight Maids-a-Milking, "
+                         "seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, "
+                         "three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+
+                         )
