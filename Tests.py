9ffb1fba2de49478cb63e29cda72b40b7964775d
wojciech mierzejewski <55141520+mierzvoj@users.noreply.github.com>
2022-12-07 18:21:28 +0100

    test range 6

--- Tests.py
+++ Tests.py
@@ -109,3 +109,19 @@ class ChristmasCarol(unittest.TestCase):
                          "On the sixth day of Christmas, my true love gave to me: six Geese-a-Laying, five Gold Rings, "
                          "four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
                          )
+
+    def test_ver_range_1_to_7(self):
+        self.assertEqual(starts_ends(0, 6),
+                         "On the first day of Christmas, my true love gave to me: a Partridge in a Pear Tree."
+                         "On the second day of Christmas, my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree."
+                         "On the third day of Christmas, my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+                         "On the fourth day of Christmas, my true love gave to me: "
+                         "four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+                         "On the fifth day of Christmas, my true love gave to me: five Gold Rings, four Calling Birds, "
+                         "three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+                         "On the sixth day of Christmas, my true love gave to me: six Geese-a-Laying, five Gold Rings, "
+                         "four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
+                         "On the seventh day of Christmas, my true love gave to me: seven Swans-a-Swimming, "
+                         "six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, "
+                         "and a Partridge in a Pear Tree."
+                         )
